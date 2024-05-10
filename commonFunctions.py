6204913b4c5f7543e685d5a4d98c2a30d818bc0e
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2024-05-10 16:19:18 +0530

    few more functions defined

--- commonFunctions.py
+++ commonFunctions.py
@@ -1,5 +1,7 @@
 # showInfo function diplays shape, # of duplicated, # of unique values, # of nan values, info of the dataframe.
 import string
+import nltk
+from nltk.stem.snowball import SnowballStemmer
 
 def showInfo(df):
     ''' input a dataframe '''
@@ -15,8 +17,31 @@ def showInfo(df):
     print()
     print('Calling info method : ')
     print(df.info())
-
+# This function takse a string and return in lower case
+def lower_casing(string_):
+    ''' input a string and return in lowercase '''
+    if len(string_.split())==1:
+        return string_
+    string_ = [s.lower() for s in string_.split()]
+    return ' '.join(string_)
+# This function takes a string and removes numeric values
+def remove_numeric(string_):
+    ''' input a string and returns a string without numeric '''
+    text = []
+    if len(string_.split())==1:
+        if string_.isnumeric()==False:
+            return string_
+    # for s in string_.split():
+    #     if s.isnumeric() == False:
+    #         text.append(s)
+    text = [s for s in string_.split() if s.isnumeric()==False]
+    return ' '.join(text)
 # This function takes a string and removes punctuations
 def remove_punctuations(string_):
     text = [s for s in string_ if s not in string.punctuation]
     return ''.join(text)
+# Stemming
+def stemming(string_):
+    stemmer = SnowballStemmer(language='english')
+    stemmed_text = [stemmer.stem(s) for s in string_.split()]
+    return ' '.join(stemmed_text)
